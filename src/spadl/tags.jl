03344ca5771fb49467bbd406e5d0b1910529990e
tlorans <thomaslorans@gmail.Com>
2021-12-26 11:42:34 +0100

    tags converted in struct

--- src/spadl/tags.jl
+++ src/spadl/tags.jl
@@ -2,68 +2,6 @@
 
 
 
-# wyscout_tags::Dict = Dict(
-#     (101 => "goal"),
-#     (102 => "own_goal"),
-#     (301 => "assist"),
-#     (302 => "key_pass"),
-#     (1901 => "counter_attack"),
-#     (401 => "left_foot"),
-#     (402 => "right_foot"),
-#     (403 => "head/body"),
-#     (1101 => "direct"),
-#     (1102 => "indirect"),
-#     (2001 => "dangerous_ball_lost"),
-#     (2101 => "blocked"),
-#     (801 => "high"),
-#     (802 => "low"),
-#     (1401 => "interception"),
-#     (1501 => "clearance"),
-#     (201 => "opportunity"),
-#     (1301 => "feint"),
-#     (1302 => "missed_ball"),
-#     (501 => "free_space_right"),
-#     (502 => "free_space_left"),
-#     (503 => "take_on_left"),
-#     (504 => "take_on_right"),
-#     (1601 => "sliding_tackle"),
-#     (601 => "anticipated"),
-#     (602 => "anticipation"),
-#     (1701 => "red_card"),
-#     (1702 => "yellow_card"),
-#     (1703 => "second_yellow_card"),
-#     (1201 => "position_goal_low_center"),
-#     (1202 => "position_goal_low_right"),
-#     (1203 => "position_goal_mid_center"),
-#     (1204 => "position_goal_mid_left"),
-#     (1205 => "position_goal_low_left"),
-#     (1206 => "position_goal_mid_right"),
-#     (1207 => "position_goal_high_center"),
-#     (1208 => "position_goal_high_left"),
-#     (1209 => "position_goal_high_right"),
-#     (1210 => "position_out_low_right"),
-#     (1211 => "position_out_mid_left"),
-#     (1212 => "position_out_low_left"),
-#     (1213 => "position_out_mid_right"),
-#     (1214 => "position_out_high_center"),
-#     (1215 => "position_out_high_left"),
-#     (1216 => "position_out_high_right"),
-#     (1217 => "position_post_low_right"),
-#     (1218 => "position_post_mid_left"),
-#     (1219 => "position_post_low_left"),
-#     (1220 => "position_post_mid_right"),
-#     (1221 => "position_post_high_center"),
-#     (1222 => "position_post_high_left"),
-#     (1223 => "position_post_high_right"),
-#     (901 => "through"),
-#     (1001 => "fairplay"),
-#     (701 => "lost"),
-#     (702 => "neutral"),
-#     (703 => "won"),
-#     (1801 => "accurate"),
-#     (1802 => "not_accurate")
-# )
-
 
 """
     WyscoutEventTags
@@ -138,15 +76,67 @@ function get_tags(events_data::Vector{WyscoutEvent})::Vector{WyscoutEventTags}
         tags = WyscoutEventTags()
 
         for j in eachindex(events_data[i].tags)
-
-            if events_data[i].tags[j]["id"] == 101
-                tags.goal = true 
-            end
-
-            if events_data[i].tags[j]["id"] == 102
-                tags.own_goal = true 
-            end
-
+            tag = events_data[i].tags[j]["id"]
+            
+            if tag == 101 tags.goal = true end
+            if tag == 102 tags.own_goal = true end
+            if tag == 301 tags.assist = true end
+            if tag == 302 tags.key_pass = true end
+            if tag == 1901 tags.counter_attack = true end
+            if tag == 401 tags.left_foot = true end
+            if tag == 402 tags.right_foot = true end
+            if tag == 403 tags.head_body = true end
+            if tag == 1101 tags.direct = true end
+            if tag == 1102 tags.indirect = true end
+            if tag == 2001 tags.dangerous_ball_lost = true end
+            if tag == 2101 tags.blocked = true end
+            if tag == 801 tags.high = true end
+            if tag == 802 tags.low = true end
+            if tag == 1401 tags.interception = true end
+            if tag == 1501 tags.clearance = true end
+            if tag == 201 tags.opportunity = true end
+            if tag == 1301 tags.feint = true end
+            if tag == 1302 tags.missed_ball = true end
+            if tag == 501 tags.free_space_right = true end
+            if tag == 502 tags.free_space_left = true end
+            if tag == 503 tags.take_on_left = true end
+            if tag == 504 tags.take_on_right = true end
+            if tag == 1601 tags.sliding_tackle = true end
+            if tag == 601 tags.anticipated = true end
+            if tag == 602 tags.anticipation = true end
+            if tag == 1701 tags.red_card = true end
+            if tag == 1702 tags.yellow_card = true end
+            if tag == 1703 tags.second_yellow_card = true end
+            if tag == 1201 tags.position_goal_low_center = true end
+            if tag == 1202 tags.position_goal_low_right = true end
+            if tag == 1203 tags.position_goal_mid_center = true end
+            if tag == 1204 tags.position_goal_mid_left = true end
+            if tag == 1205 tags.position_goal_low_left = true end
+            if tag == 1206 tags.position_goal_mid_right = true end
+            if tag == 1207 tags.position_goal_high_center = true end
+            if tag == 1208 tags.position_goal_high_left = true end
+            if tag == 1209 tags.position_goal_high_right = true end
+            if tag == 1210 tags.position_out_low_right = true end
+            if tag == 1211 tags.position_out_mid_left = true end
+            if tag == 1212 tags.position_out_low_left = true end
+            if tag == 1213 tags.position_out_mid_rights = true end
+            if tag == 1214 tags.position_out_high_center = true end
+            if tag == 1215 tags.position_out_high_left = true end
+            if tag == 1216 tags.position_out_high_right = true end
+            if tag == 1217 tags.position_post_low_right = true end
+            if tag == 1218 tags.position_post_mid_left = true end
+            if tag == 1219 tags.position_post_low_left = true end
+            if tag == 1220 tags.position_post_mid_right = true end
+            if tag == 1221 tags.position_post_high_center = true end
+            if tag == 1222 tags.position_post_high_left = true end
+            if tag == 1223 tags.position_post_high_right = true end
+            if tag == 901 tags.through = true end
+            if tag == 1001 tags.fairplay = true end
+            if tag == 701 tags.lost = true end
+            if tag == 702 tags.neutral = true end
+            if tag == 703 tags.won = true end
+            if tag == 1801 tags.accurate = true end
+            if tag == 1802 tags.not_accurate = true end
 
         end
 
